29bd075b11d4d042290ea0515465a42242c784c2
ROM-PacMe <rafy.pachecomendez@gmail.com>
2015-12-04 19:00:57 -0300

    main: cleanup cooperve device tree (thanks Epirex)

---
+++ cm.mk
@@ -0,0 +1,24 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Inherit some common cyanogenmod stuff.
+$(call inherit-product, vendor/cm/config/common_mini_phone.mk)
+
+# Inherit device configuration for cooperve
+$(call inherit-product, device/samsung/cooperve/full_cooperve.mk)
+
+# Inherit some common CM stuff.
+TARGET_SCREEN_HEIGHT := 480
+TARGET_SCREEN_WIDTH := 320
+$(call inherit-product, vendor/cm/config/mini.mk)
+
+# Overrides
+PRODUCT_NAME := cm_cooperve
+PRODUCT_DEVICE := cooperve
+PRODUCT_BRAND := Samsung
+PRODUCT_MODEL := GT-S5830i
+PRODUCT_MANUFACTURER := Samsung
+PRODUCT_CHARACTERISTICS := phone
+
+PRODUCT_RELEASE_NAME := GalaxyCooperVE
+PRODUCT_VERSION_DEVICE_SPECIFIC := GT-S5830i
---
+++ device_cooperve.mk
@@ -0,0 +1,35 @@
+# Copyright (C) 2010 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Enable repeatable keys in CWM
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.cwm.enable_key_repeat=true \
+    ro.cwm.repeatable_keys=114,115
+
+# Torch
+PRODUCT_PACKAGES += \
+    Torch
+
+# MDPI assets
+PRODUCT_AAPT_CONFIG := normal mdpi
+PRODUCT_AAPT_PREF_CONFIG := mdpi
+
+$(call inherit-product, device/mdpi-common/mdpi.mk)
+
+# Inherit products
+$(call inherit-product, device/samsung/bcm21553-common/common.mk)
+$(call inherit-product, vendor/samsung/cooperve/vendor.mk)
+
+# Add device package overlay
+DEVICE_PACKAGE_OVERLAYS += device/samsung/cooperve/overlay
